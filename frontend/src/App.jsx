f3a4fc72c538b03a5fe59d9b23f255108d226085
ViktorBazsi <viktor.balazs.endre@gmail.com>
2025-01-03 13:57:32 +0100

    COMPLETED - LoginPage, AuthContext, UserValidationSchema

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <div>SKELETON</div>;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthContext';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <div>
+          <Routes>
+            <Route path="login" element={<LoginPage />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
+  );
 }
+
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'theatron01-pattern': "url('Public/theatron01.jpg')",
+        'theatron02-pattern': "url('Public/theatron02.jpg')",
+      },
+    },
   },
   plugins: [],
 };
