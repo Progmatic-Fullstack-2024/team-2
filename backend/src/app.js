24c318f2e1e9c247c9bd883d6b05887367221613
Szebenia <atis.8021@gmail.com>
2025-01-04 14:04:32 +0100

    dotenv fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/error-handler.middleware.js";
 import authRoutes from "./routes/auth.routes.js";
-import { VITE_API_URL } from "./constants/constants.js";
+import { FRONTEND_URL } from "./constants/constants.js";
 
 const app = express();
 
-app.use(cors({ origin: VITE_API_URL }));
+app.use(cors({ origin: FRONTEND_URL }));
 
 app.use(express.json());
 
