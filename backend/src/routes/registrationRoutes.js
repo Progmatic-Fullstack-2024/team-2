0d11f41ebcef99aa2fab8257e593e39aec57934c
AnitaM75 <nagy.maria.anita75@gmail.com>
2025-01-04 09:56:41 +0100

    clean backend

--- backend/src/routes/registrationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import registrationController from '../controllers/registrationController.js';
-
-const router = express.Router();
-
-// Regisztrációs végpont
-router.post('/register', registrationController.registerUser);
-
-export default router;
