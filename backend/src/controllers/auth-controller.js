9befe4355002fe9b3d4becfb3cbb52bef0986f80
ViktorBazsi <viktor.balazs.endre@gmail.com>
2025-01-04 12:40:08 +0100

    DONE - working registration and login

--- backend/src/controllers/auth-controller.js
+++ backend/src/controllers/auth-controller.js
@@ -1,25 +1,24 @@
-
 import authService from "../services/auth-service.js";
 import { userValidationSchemaForLogin } from "../validations/userValidationSchema.js";
 
-async function registration(req,res) {
-    const{firstName,lastName,email,password,phone}=req.body;
-    if(firstName && lastName && email && password) {
-        const newUser=await authService.userCreate(lastName,firstName,email,password,phone);
-        if(newUser) {
-            res.status(201).send("user recorded succesfully"); 
-            console.log(newUser);
-        }
-             
-        else res.status(500).send("server error");
-    }
-    else {
-        res.status(401).send("Data is incomplete");
-    }
+async function registration(req, res) {
+  const { firstName, lastName, email, password, phone } = req.body;
+  if (firstName && lastName && email && password) {
+    const newUser = await authService.userCreate(
+      lastName,
+      firstName,
+      email,
+      password,
+      phone,
+    );
+    if (newUser) {
+      res.status(201).send("user recorded succesfully");
+      console.log(newUser);
+    } else res.status(500).send("server error");
+  } else {
+    res.status(401).send("Data is incomplete");
+  }
 }
-
-
-
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
@@ -32,4 +31,4 @@ const login = async (req, res, next) => {
   }
 };
 
-export default {registration, login };
+export default { registration, login };
--- backend/src/routes/auth-routes.js
+++ backend/src/routes/auth-routes.js
@@ -1,15 +1,10 @@
-
-import express from "express"; 
-import { formData } from "../middlewares/multer-middleware.js";
-import authControll from "../controllers/auth-controller.js";
-
-const router = express.Router();
-
-router.post("/registration", formData.none(),authControll.registration);
 import express from "express";
+import { formData } from "../middlewares/multer-middleware.js";
 import authController from "../controllers/auth-controller.js";
 
 const router = express.Router();
+
+router.post("/registration", formData.none(), authController.registration);
 
 router.post("/login", authController.login);
 
