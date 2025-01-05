8563efe12ada4b8c65dae8d788669dcce03db9e4
tMas9941 <tamasm9941@gmail.com>
2025-01-05 20:30:56 +0100

    nothing important

--- backend/src/middlewares/error-handler.middleware.js
+++ backend/src/middlewares/error-handler.middleware.js
@@ -1,10 +1,7 @@
 import HttpError from "../utils/HttpError.js";
 
-export const errorHandler = (err, req, res) => {
-	if (err instanceof HttpError) {
-		console.log("err : ", err);
-		return res.status(err.status).json({ error: err.message });
-	}
+export const errorHandler = (err, req, res, next) => {
+	if (err instanceof HttpError) res.status(err.status).json({ error: err.message });
 	console.log("Internal Server Error", err);
-	res.status(500).json({ error: "Internal Server Error" });
+	return res.status(500).json({ error: "Internal Server Error" });
 };
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -4,43 +4,36 @@ import prisma from "../models/prisma-client.js";
 import { JWT_SECRET } from "../constants/constants.js";
 import HttpError from "../utils/HttpError.js";
 
-const registration = async ({
-  lastName,
-  firstName,
-  email,
-  password,
-  phone,
-  role = "user",
-}) => {
-  const emailExists = await prisma.user.findUnique({ where: { email } });
-  if (emailExists) throw new HttpError("Email already exists!", 403);
+const registration = async ({ lastName, firstName, email, password, phone, role = "user" }) => {
+	const emailExists = await prisma.user.findUnique({ where: { email } });
+	if (emailExists) throw new HttpError("Email already exists!", 403);
 
-  const hashedPassword = await bcrypt.hash(password, 5);
-  const newUser = await prisma.user.create({
-    data: { lastName, firstName, email, password: hashedPassword, phone, role },
-  });
+	const hashedPassword = await bcrypt.hash(password, 5);
+	const newUser = await prisma.user.create({
+		data: { lastName, firstName, email, password: hashedPassword, phone, role },
+	});
 
-  return newUser;
+	return newUser;
 };
 
 const login = async ({ email, password }) => {
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) throw new HttpError("Invalid email or password!", 403);
+	const user = await prisma.user.findUnique({ where: { email } });
+	if (!user) throw new HttpError("Invalid email or password!", 403);
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) throw new HttpError("Invalid email or password!", 403);
+	const isPasswordValid = await bcrypt.compare(password, user.password);
+	if (!isPasswordValid) throw new HttpError("Invalid email or password!", 403);
 
-  const payload = {
-    id: user.id,
-    email: user.email,
-    lastName: user.lastName,
-    firstName: user.firstName,
-    isAdmin: user.isAdmin,
-  };
+	const payload = {
+		id: user.id,
+		email: user.email,
+		lastName: user.lastName,
+		firstName: user.firstName,
+		isAdmin: user.isAdmin,
+	};
 
-  const token = jwt.sign(payload, JWT_SECRET);
+	const token = jwt.sign(payload, JWT_SECRET);
 
-  return token;
+	return token;
 };
 
 export default { registration, login };
