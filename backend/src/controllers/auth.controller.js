95334f9dda47b1210f0c0e0857247c563461526c
pelaszlo <pelaszlo@gmail.hu>
2025-01-11 23:01:44 +0100

    Lint ellenőrzés

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,72 +31,82 @@ const login = async (req, res, next) => {
   }
 };
 
-const listUsers=async(req,res,next)=>{
-  try{
-      const users=await authService.getAllUser();
-      res.json(users);
-  } catch(error){
-      next(error);
+const listUsers = async (req, res, next) => {
+  try {
+    const users = await authService.getAllUser();
+    res.json(users);
+  } catch (error) {
+    next(error);
   }
 };
 
-const getUser=async(req,res,next)=>{
-  try{
-    const id=req.params.id;
+const getUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
     if (id) {
-      const user=await authService.getUserById(id);
-      if(!user) next(new HttpError("User is not Found",404));
+      const user = await authService.getUserById(id);
+      if (!user) next(new HttpError("User is not Found", 404));
       else res.json(user);
+    }
+  } catch (error) {
+    next(error);
   }
-
-} catch(error){
-    next(error);
-}
 };
 
-const getOwnUser=async(req,res,next)=>{
-  try{
-      const id=  req.user.id;
-      const user=await authService.getOwnUserById(id);
-      if(!user) next(new HttpError("User is not Found",404));
-      else res.json(user);
-  
-
-} catch(error){
+const getOwnUser = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const user = await authService.getOwnUserById(id);
+    if (!user) next(new HttpError("User is not Found", 404));
+    else res.json(user);
+  } catch (error) {
     next(error);
-}
+  }
 };
 
-const updateUser=async(req,res,next)=>{
-  let{id,firstName,lastName,email,phone,password,role}=req.body;
- 
-  
-  if(id) {
-    if(req.user.role!="Admin" && id!=req.user.id) {
-      next(new HttpError("Access denied: Admin can update users only",403));
+const updateUser = async (req, res, next) => {
+  const { id, firstName, lastName, email, phone } = req.body;
+  let { password, role } = req.body;
+  if (id) {
+    if (req.user.role !== "Admin" && id !== req.user.id) {
+      next(new HttpError("Access denied: Admin can update users only", 403));
       return;
     }
-    if (req.user.role!="Admin") role=undefined;
-    else if (id!=req.user.id) password=undefined;
-    try{
-    const user=await authService.updateUser(id,firstName,lastName,email,phone,role,password);
-    if (user) res.status(201).json({message:"Update is successful."});
-    else next(new HttpError("User is not Found",404));
-  } catch(error){
-    next(error);
-  }
-}
-else next(new HttpError("User is not Found",404));
+    if (req.user.role !== "Admin") role = undefined;
+    else if (id !== req.user.id) password = undefined;
+    try {
+      const user = await authService.updateUser(
+        id,
+        firstName,
+        lastName,
+        email,
+        phone,
+        role,
+        password,
+      );
+      if (user) res.status(201).json({ message: "Update is successful." });
+      else next(new HttpError("User is not Found", 404));
+    } catch (error) {
+      next(error);
+    }
+  } else next(new HttpError("User is not Found", 404));
 };
 
-const deleteUser=async(req,res,next)=>{
-    const id=req.params.id;
-     if (id) {
-      const user=await authService.deleteUser(id);
-      if (user) res.status(201).json({message:"User deleted"});
-      else next(new HttpError("User is not Found",404));
-     }
-     else next(new HttpError("User id is required",401));
-}
+const deleteUser = async (req, res, next) => {
+  const { id } = req.params;
+  if (id) {
+    const user = await authService.deleteUser(id);
+    if (user) res.status(201).json({ message: "User deleted" });
+    else next(new HttpError("User is not Found", 404));
+  } else next(new HttpError("User id is required", 401));
+};
 
-export default { registration, login, listUsers, getUser, getOwnUser, updateUser,deleteUser };
+export default {
+  registration,
+  login,
+  listUsers,
+  getUser,
+  getOwnUser,
+  updateUser,
+  deleteUser,
+};
