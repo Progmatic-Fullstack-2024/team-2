058f5188ce1b83a44abb75086049e1f82f269725
pelaszlo <pelaszlo@gmail.hu>
2025-01-11 23:03:15 +0100

    lint javitas

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -7,10 +7,10 @@ const router = express.Router();
 
 router.post("/registration", formData.none(), authController.registration);
 router.post("/login", formData.none(), authController.login);
-router.get ("/users/own",isAuthenticated,authController.getOwnUser);
-router.get ("/users/:id",isAdmin,authController.getUser);
-router.get ("/users",isAdmin,authController.listUsers);
-router.patch("/users",isAuthenticated,authController.updateUser);
-router.delete("/users/:id",isAdmin,authController.deleteUser)
+router.get("/users/own", isAuthenticated, authController.getOwnUser);
+router.get("/users/:id", isAdmin, authController.getUser);
+router.get("/users", isAdmin, authController.listUsers);
+router.patch("/users", isAuthenticated, authController.updateUser);
+router.delete("/users/:id", isAdmin, authController.deleteUser);
 
 export default router;
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -4,11 +4,10 @@ import prisma from "../models/prisma-client.js";
 import { JWT_SECRET } from "../constants/constants.js";
 import HttpError from "../utils/HttpError.js";
 
-const getEmailExists = async (email)=>{
-  const emailExists=await prisma.user.findUnique({ where: { email } });
+const getEmailExists = async (email) => {
+  const emailExists = await prisma.user.findUnique({ where: { email } });
   return emailExists;
-}
-
+};
 
 const registration = async ({
   lastName,
@@ -47,69 +46,82 @@ const login = async ({ email, password }) => {
   const token = jwt.sign(payload, JWT_SECRET);
 
   return token;
-}
-
-const getAllUser=async()=>{
-    const users=await prisma.user.findMany({
-      select:{id:true,
-        lastName:true,
-        firstName:true,
-        email:true,
-        phone:true,
-        role:true,
-      }
-    });
-    return users;
 };
 
-const getUserById=async(id)=>{
-  const user=await prisma.user.findUnique({
-    where:{id},
-    select:{id:true,
-      lastName:true,
-      firstName:true,
-      email:true,
-      phone:true,
-      role:true,
-    }
+const getAllUser = async () => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      lastName: true,
+      firstName: true,
+      email: true,
+      phone: true,
+      role: true,
+    },
+  });
+  return users;
+};
+
+const getUserById = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      lastName: true,
+      firstName: true,
+      email: true,
+      phone: true,
+      role: true,
+    },
   });
   return user;
 };
 
-const getOwnUserById=async(id)=>{
-  const user=await getUserById(id);
-  if (user.role=="user") delete user.role;
+const getOwnUserById = async (id) => {
+  const user = await getUserById(id);
+  if (user.role === "user") delete user.role;
   return user;
 };
 
-const updateUser=async(id,firstName,lastName,email,phone,role,password)=>{
-  
-  let hashedPassword=undefined;
-  if (password)  hashedPassword = await bcrypt.hash(password, 5);
+const updateUser = async (
+  id,
+  firstName,
+  lastName,
+  email,
+  phone,
+  role,
+  password,
+) => {
+  let hashedPassword;
+  if (password) hashedPassword = await bcrypt.hash(password, 5);
   if (email) {
-      const existEmail= await getEmailExists(email);
-      if (existEmail && existEmail.id!=id) throw new HttpError("Email already exists!", 403);
+    const existEmail = await getEmailExists(email);
+    if (existEmail && existEmail.id !== id)
+      throw new HttpError("Email already exists!", 403);
   }
-  const user=await prisma.user.update({
-    where:{id},
-    data:{firstName,
-      lastName,
-      email,
-      phone,
-      role,
-      password:hashedPassword}
+  const user = await prisma.user.update({
+    where: { id },
+    data: { firstName, lastName, email, phone, role, password: hashedPassword },
   });
   return user;
 };
 
-const deleteUser=async(id)=>{
-  let user=await getUserById(id);
+const deleteUser = async (id) => {
+  let user = await getUserById(id);
   if (user)
-    user=await prisma.user.delete({
-    where:{id},
+    user = await prisma.user.delete({
+      where: { id },
     });
-  
-  return user;
-}
 
-export default { registration, login, getAllUser, getUserById,getOwnUserById, updateUser,deleteUser};
+  return user;
+};
+
+export default {
+  registration,
+  login,
+  getAllUser,
+  getUserById,
+  getOwnUserById,
+  updateUser,
+  deleteUser,
+};
