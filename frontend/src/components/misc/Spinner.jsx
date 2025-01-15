0a03bd5171ba1d97f2edc3b31d28c87f465089ea
tMas9941 <tamasm9941@gmail.com>
2025-01-15 18:37:04 +0100

    nothing

--- frontend/src/components/misc/Spinner.jsx
+++ frontend/src/components/misc/Spinner.jsx
@@ -1,4 +1,4 @@
-export default function Spinner({ size = 20, color = 'white' }) {
+export default function Spinner({ size = 12, color = 'white' }) {
   return (
     <div
       className={`inline-block h-${size} w-${size} animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-${color}`}
