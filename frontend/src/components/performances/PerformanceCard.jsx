265988adbf6bd53dd80a241cd2307e8e8044288a
tMas9941 <tamasm9941@gmail.com>
2025-01-17 13:50:27 +0100

    fix after merge

--- frontend/src/components/performances/PerformanceCard.jsx
+++ frontend/src/components/performances/PerformanceCard.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 
@@ -6,18 +5,9 @@ import PerformanceCardEmpty from './PerformanceCardEmpty';
 import DefaultButton from '../misc/DefaultButton';
 
 export default function PerformanceCard({ data }) {
-  const gradientClassTop = `bg-gradient-to-b from-black/80  to-transparent to-70% absolute h-1/3 w-full  tablet:rounded-t-2xl`;
-  const gradientClassBot = `bottom-0 bg-gradient-to-b from-transparent to-black/80 to-50% absolute h-2/3 w-full tablet:rounded-b-lg `;
-
   const rendered = useRef(false); // stops unnecessary rerender of imageReady state
   const [imageReady, setImageReady] = useState(false);
-
-  // BAZSI RÉSZE:
   const navigate = useNavigate();
-
-  const handleNavigate = () => {
-    navigate(`/performances/${data.id}`);
-  };
 
   async function fetchImageAndCache(url) {
     try {
@@ -54,22 +44,23 @@ export default function PerformanceCard({ data }) {
         backgroundImage: `url(${localStorage.getItem(imageReady)})`,
       }}
     >
-      <div className={gradientClassTop} />
-      <div className={gradientClassBot} />
+      <div
+        id="top-gradient"
+        className={`bg-gradient-to-b from-black/80  to-transparent to-70% absolute h-1/3 w-full  tablet:rounded-t-2xl`}
+      />
+      <div
+        id="bot-gradient"
+        className={`bottom-0 bg-gradient-to-b from-transparent to-black/80 to-50% absolute h-2/3 w-full tablet:rounded-b-lg `}
+      />
       <h1 className="z-10 mx-5 mt-3 text-2xl font-semibold">{data.title}</h1>
       <div className="z-10 mx-5 mb-5 flex flex-col bot-0 ">
-        <span className="self-end text-2xl font-bold">{data.price} Ft/fő</span>
-        <span>Helyszín : </span>
-        <span>Közeműködők : </span>
-        <span className="mb-3">Időpont : </span>
-        <DefaultButton onClick={handleNavigate} text="Érdekel..." />
         <span className="self-end text-3xl text-c-secondary-light font-bold mb-2">
           {data.price} <span className="text-white text-xl">Ft/fő</span>
         </span>
-        {/* <span>Helyszín : </span> */}
-        {/* <span>Közeműködők : </span> */}
+        <span>Helyszín : </span>
+        <span>Közeműködők : </span>
         <span className="mb-3">Időpont : {data.performanceDate[0]}</span>
-        <DefaultButton text="Érdekel..." />
+        <DefaultButton onClick={() => navigate(`/performances/${data.id}`)} text="Érdekel..." />
       </div>
     </div>
   );
