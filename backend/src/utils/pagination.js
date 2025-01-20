46fa6e86f55e7742c26fdca192e6b843cbee513c
tMas9941 <tamasm9941@gmail.com>
2025-01-20 18:17:35 +0100

    lint fix

--- backend/src/utils/pagination.js
+++ backend/src/utils/pagination.js
@@ -1,17 +1,24 @@
-export function paginate(query, defaultLimit = 10, defaultPage = 1) {
-	const { page = defaultPage, limit = defaultLimit, filter, orderBy, sort } = query;
+export default function paginate(query, defaultLimit = 10, defaultPage = 1) {
+  const {
+    page = defaultPage,
+    limit = defaultLimit,
+    filter,
+    orderBy,
+    sort,
+  } = query;
 
-	const pagination = {
-		skip: (page - 1) * limit,
-		take: parseInt(limit),
-	};
+  const pagination = {
+    skip: (page - 1) * limit,
+    take: Number(limit),
+  };
 
-	if (filter)
-		pagination.where = {
-			[filter[0]]: { contains: filter[1], mode: "insensitive" },
-		};
+  if (filter) {
+    pagination.where = {
+      [filter[0]]: { contains: filter[1], mode: "insensitive" },
+    };
+  }
 
-	if (orderBy) pagination.orderBy = { [orderBy]: sort || "asc" };
+  if (orderBy) pagination.orderBy = { [orderBy]: sort || "asc" };
 
-	return pagination;
+  return pagination;
 }
