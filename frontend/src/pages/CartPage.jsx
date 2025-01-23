c0178553e16ad074610fc78dd9b64087c3420040
AnitaM75 <nagy.maria.anita75@gmail.com>
2025-01-23 19:36:09 +0100

    Cart page background remove

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -7,7 +7,7 @@ export default function CartPage() {
 
   if (!user) {
     return (
-      <div className="bg-theatron02-pattern bg-cover min-h-screen flex flex-col items-center">
+      <div className=" min-h-screen flex flex-col items-center">
         <ImageTitle title="Kosár" subtitle="Jelentkezz be a kosarad megtekintéséhez!" />
         <p className="text-white mt-5">A kosár megtekintéséhez be kell jelentkezni.</p>
       </div>
@@ -15,11 +15,8 @@ export default function CartPage() {
   }
 
   return (
-    <div className="bg-theatron02-pattern bg-cover min-h-screen flex flex-col items-center">
-      <ImageTitle
-        title="Kosár"
-        description="Válaszd ki a jegyedet!"
-      />
+    <div className=" min-h-screen flex flex-col items-center">
+      <ImageTitle title="Kosár" description="Válaszd ki a jegyedet!" />
       <Cart />
     </div>
   );
