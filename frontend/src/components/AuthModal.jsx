e601c600885d222735ce3825ae1257d776a40d1d
AnitaM75 <nagy.maria.anita75@gmail.com>
2025-02-01 19:25:19 +0100

    Redy Modal lint ok

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import LoginForm from './LoginForm';
 import RegistrationForm from './RegistrationForm';
 
-export default function AuthModal({ onClose, defaultForm = 'login' }) {
-  const [formType, setFormType] = useState(defaultForm);
+export default function AuthModal({ onClose, formType = 'login' }) {
+  const [currentForm, setCurrentForm] = useState(formType);
+
+  useEffect(() => {
+    setCurrentForm(formType);
+  }, [formType]); 
 
   const switchForm = (newForm) => {
-    setFormType(newForm);
+    setCurrentForm(newForm);
   };
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div
         className={`rounded overflow-y-auto mx-4 ${
-          formType === 'register' ? ' w-full  ' : 'max-w-sm'
+          currentForm === 'register' ? ' w-full  ' : 'max-w-sm'
         }`}
       >
         <div>
-          {formType === 'login' ? (
-            <LoginForm onSwitch={switchForm} />
+          {currentForm === 'login' ? (
+            <LoginForm onSwitch={switchForm} onClose={onClose} />
           ) : (
             <RegistrationForm onSwitch={switchForm} />
           )}
--- frontend/src/components/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm.jsx
@@ -1,14 +1,15 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import AuthResult from './AuthResult';
 import AuthContext from '../contexts/AuthContext';
 import { userValidationSchemaForRegistration } from '../schema/userValidationSchema';
 import DefaultButton from './misc/DefaultButton';
 
 export default function RegistrationForm({ onSwitch }) {
-  const { register, showAuthMsg, authMsg } = useContext(AuthContext);
+  const { register } = useContext(AuthContext);
+  const [successMsg, setSuccessMsg] = useState(false);
+
   const initialValues = {
     firstName: '',
     lastName: '',
@@ -18,126 +19,134 @@ export default function RegistrationForm({ onSwitch }) {
     confirmPassword: '',
   };
 
-  const handleRegister = async (values) => {
-    showAuthMsg(true);
-    await register(values);
+  const handleRegister = async (values, { resetForm }) => {
+    const success = await register(values);
+
+    if (success) {
+      setSuccessMsg(true);
+      resetForm();
+      setTimeout(() => {
+        setSuccessMsg(false);
+        if (onSwitch) {
+          onSwitch('login');
+        }
+      }, 3000);
+    }
   };
 
   return (
     <div className="w-1/2 mx-auto my-40 bg-c-secondary-light p-12 rounded-md">
       <h2 className="font-bold text-gray-800 text-2xl mb-6">Regisztráció</h2>
-      {authMsg.show ? (
-        <AuthResult params={{ navigateTo: '/' }} />
-      ) : (
-        <>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={userValidationSchemaForRegistration}
-            onSubmit={handleRegister}
-          >
-            <Form>
-              <div className="mb-4">
-                <label htmlFor="lastName" className="text-gray-800 font-bold">
-                  Vezetéknév <span className="text-red-500">*</span>
-                </label>
-                <Field
-                  type="text"
-                  name="lastName"
-                  placeholder="Add meg a vezetéknevedet"
-                  className="w-full border p-2 rounded my-1 text-gray-800"
-                />
-                <ErrorMessage name="lastName" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="firstName" className="text-gray-800 font-bold">
-                  Keresztnév <span className="text-red-500">*</span>
-                </label>
-                <Field
-                  type="text"
-                  name="firstName"
-                  placeholder="Add meg a keresztnevedet"
-                  className="w-full border p-2 rounded my-1 text-gray-800"
-                />
-                <ErrorMessage name="firstName" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="E-mail" className="text-gray-800 font-bold">
-                  E-mail cím <span className="text-red-500">*</span>
-                </label>
-                <Field
-                  type="email"
-                  name="email"
-                  placeholder="Add meg az e-mail címedet"
-                  className="w-full border p-2 rounded my-1 text-gray-800"
-                />
-                <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="phone" className="text-gray-800 font-bold">
-                  Telefonszám
-                </label>
-                <Field
-                  type="text"
-                  name="phone"
-                  placeholder="Add meg a telefonszámodat"
-                  className="w-full border p-2 rounded my-1 text-gray-800"
-                />
-                <ErrorMessage name="phone" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="password" className="text-gray-800 font-bold">
-                  Jelszó <span className="text-red-500">*</span>
-                </label>
-                <Field
-                  type="password"
-                  name="password"
-                  placeholder="Adj meg egy jelszót min 6 karakter"
-                  className="w-full border p-2 rounded my-1 text-gray-800"
-                />
-                <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="confirmPassword" className="text-gray-800 font-bold">
-                  Jelszó megerősítése<span className="text-red-500"> *</span>
-                </label>
-                <Field
-                  type="password"
-                  name="confirmPassword"
-                  placeholder="Add meg a jelszót mégegyszer"
-                  className="w-full border p-2 rounded my-1 text-gray-800"
-                />
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
-              </div>
-              <div className="flex justify-center">
-                <DefaultButton text="Regisztráció" type="submit" />
-              </div>
-            </Form>
-          </Formik>
-          <div className="flex justify-center mt-5">
-            Már regisztráltál?
-            {onSwitch ? (
-              <button
-                type="button"
-                onClick={() => onSwitch('login')}
-                className="text-gray-500 hover:underline pl-2 hover:scale-110 hover:text-blue-800 transition duration-700"
-              >
-                Kérlek jelentkezz be
-              </button>
-            ) : (
-              <Link
-                to="/login"
-                className="text-gray-500 hover:underline pl-2 hover:scale-110 hover:text-blue-800 transition duration-700"
-              >
-                Kérlek jelentkezz be
-              </Link>
-            )}
-          </div>
-        </>
+
+      {successMsg && (
+        <div className="text-green-600 font-bold text-center mb-4">
+          ✅ Sikeres regisztráció! Átirányítás a bejelentkezéshez...
+        </div>
       )}
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={userValidationSchemaForRegistration}
+        onSubmit={handleRegister}
+      >
+        <Form>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="text-gray-800 font-bold">
+              Vezetéknév <span className="text-red-500">*</span>
+            </label>
+            <Field
+              type="text"
+              name="lastName"
+              placeholder="Add meg a vezetéknevedet"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage name="lastName" component="div" className="text-red-500 text-sm" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="firstName" className="text-gray-800 font-bold">
+              Keresztnév <span className="text-red-500">*</span>
+            </label>
+            <Field
+              type="text"
+              name="firstName"
+              placeholder="Add meg a keresztnevedet"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage name="firstName" component="div" className="text-red-500 text-sm" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="text-gray-800 font-bold">
+              E-mail cím <span className="text-red-500">*</span>
+            </label>
+            <Field
+              type="email"
+              name="email"
+              placeholder="Add meg az e-mail címedet"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="phone" className="text-gray-800 font-bold">
+              Telefonszám
+            </label>
+            <Field
+              type="text"
+              name="phone"
+              placeholder="Add meg a telefonszámodat"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage name="phone" component="div" className="text-red-500 text-sm" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="text-gray-800 font-bold">
+              Jelszó <span className="text-red-500">*</span>
+            </label>
+            <Field
+              type="password"
+              name="password"
+              placeholder="Adj meg egy jelszót min 6 karakter"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="confirmPassword" className="text-gray-800 font-bold">
+              Jelszó megerősítése<span className="text-red-500"> *</span>
+            </label>
+            <Field
+              type="password"
+              name="confirmPassword"
+              placeholder="Add meg a jelszót mégegyszer"
+              className="w-full border p-2 rounded my-1 text-gray-800"
+            />
+            <ErrorMessage name="confirmPassword" component="div" className="text-red-500 text-sm" />
+          </div>
+          <div className="flex justify-center">
+            <DefaultButton text="Regisztráció" type="submit" />
+          </div>
+        </Form>
+      </Formik>
+
+      <div className="flex justify-center mt-5">
+        Már regisztráltál?
+        {onSwitch ? (
+          <button
+            type="button"
+            onClick={() => onSwitch('login')}
+            className="text-gray-500 hover:underline pl-2 hover:scale-110 hover:text-blue-800 transition duration-700"
+          >
+            Kérlek jelentkezz be
+          </button>
+        ) : (
+          <Link
+            to="/login"
+            className="text-gray-500 hover:underline pl-2 hover:scale-110 hover:text-blue-800 transition duration-700"
+          >
+            Kérlek jelentkezz be
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
