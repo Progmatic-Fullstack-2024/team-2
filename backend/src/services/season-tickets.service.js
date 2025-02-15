bbf660f1039cc5792e7698607a7edb8bcea5abfc
tMas9941 <tamasm9941@gmail.com>
2025-02-15 14:02:36 +0100

    remove console log

--- backend/src/services/season-tickets.service.js
+++ backend/src/services/season-tickets.service.js
@@ -1,22 +1,22 @@
 import prisma from "../models/prisma-client.js";
 
 const list = async () => {
-  const response = await prisma.seasonTicket.findMany({
-    orderBy: { price: "asc" },
-  });
-  console.log("response");
-  return response;
+	const response = await prisma.seasonTicket.findMany({
+		orderBy: { price: "asc" },
+	});
+
+	return response;
 };
 
 const create = async ({ name, price, durationDay, seatQuantity }) => {
-  const response = await prisma.seasonTicket.create({
-    data: { name, price, durationDay, seatQuantity },
-  });
-  return response;
+	const response = await prisma.seasonTicket.create({
+		data: { name, price, durationDay, seatQuantity },
+	});
+	return response;
 };
 
 const getById = async ({ id }) => {
-  const response = await prisma.seasonTicket.findUnique({ where: { id } });
-  return response;
+	const response = await prisma.seasonTicket.findUnique({ where: { id } });
+	return response;
 };
 export default { list, create, getById };
