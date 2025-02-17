8ac721e01ad4cb6e74ca82e0946e4422bcc5d03d
ViktorBazsi <viktor.balazs.endre@gmail.com>
2025-02-17 02:30:26 +0100

    REDONE - landingPage (about us ver1)

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 // Components
+import AuthModal from '../components/AuthModal';
+import DefaultButton from '../components/misc/DefaultButton';
 import ImageTitle from '../components/misc/ImageTitle';
-// import PerformancesList from '../components/performances/PerformancesList';
 import ListingFuturePerformances from '../components/performances/ListingFuturePerformances';
 import PerformancesByTheaters from '../components/performances/PerformancesByTheaters';
 import PerformancesNextWeek from '../components/performances/PerformancesNextWeek';
@@ -12,7 +13,9 @@ import performancesService from '../services/performances.service';
 export default function PerformancesPage() {
   const [performances, setPerformances] = useState();
   const [searchParams] = useSearchParams();
-  const rendered = useRef(false); // stops unnecessary rerender of performances state
+  const rendered = useRef(false);
+  const navigate = useNavigate();
+  const [isAuthModalOpen, setAuthModalOpen] = useState(false);
 
   const getPeformances = async () => {
     const data = await performancesService.listAll();
@@ -33,19 +36,70 @@ export default function PerformancesPage() {
 
   return (
     <>
-      <ImageTitle
-        title="Theatron"
-        description="Üdvözlünk a világ első színházi bérlet applikációjában! Válaszd ki a darabot, a helyed és már mehetsz is!"
-      />
+      {/* Bevezető szöveg */}
+      <ImageTitle title="BreakThe4th" description="Minden, ami független! Minden, ami színház!" />
 
-      <div className="w-full max-w-screen-desktop flex flex-col items-center mx-auto ">
-        {performances ? <PerformancesNextWeek performances={performances} /> : null}
+      {/* Hogyan működik? */}
+      <div className="max-w-6xl w-full mx-auto bg-c-secondary-dark bg-opacity-10 shadow-lg rounded-2xl p-12 my-16 px-6 sm:px-12 lg:px-20">
+        <h2 className="text-4xl font-bold text-c-text mb-6">Hogyan működik?</h2>
+        <p className="text-lg text-c-text/80 mb-6">
+          Először is, lépj be! <br /> Vagy, ha még nem tetted, regisztrálj!
+        </p>
+        <DefaultButton onClick={() => setAuthModalOpen(true)} text="Belépek!" />
       </div>
-      <div className="w-full max-w-screen-desktop flex flex-col items-center mx-auto">
-        {performances ? <PerformancesByTheaters performances={performances} /> : null}
+
+      {/* AuthModal megjelenítése */}
+      {isAuthModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <AuthModal onClose={() => setAuthModalOpen(false)} />
+        </div>
+      )}
+
+      {/* Böngészés */}
+      <div className="max-w-6xl w-full mx-auto bg-c-secondary-dark bg-opacity-10 shadow-lg rounded-2xl p-12 my-16 px-6 sm:px-12 lg:px-20">
+        <h2 className="text-4xl font-bold text-c-text mb-6">Böngészd az előadásokat!</h2>
+        <p className="text-lg text-c-text/80 mb-6">
+          Egy helyen találod Magyarország legszínesebb független színházi kínálatát.
+        </p>
+        <DefaultButton onClick={() => navigate('/browse')} text="Böngéssz!" />
+        {performances && <PerformancesByTheaters performances={performances} />}
       </div>
-      <div className="w-full max-w-screen-desktop flex flex-col items-center mx-auto">
-        {performances ? <ListingFuturePerformances performances={performances} /> : null}
+
+      {/* Előadások a következő héten */}
+      <div className="max-w-6xl w-full mx-auto bg-c-secondary-dark bg-opacity-10 shadow-lg rounded-2xl p-12 my-16 px-6 sm:px-12 lg:px-20">
+        <h2 className="text-4xl font-bold text-c-text mb-6">
+          Izgalmas! Személyre szabott ajánlások!
+        </h2>
+        <p className="text-lg text-c-text/80 mb-6">
+          Nézd meg az elkövetkező napok legjobb előadásait, és foglalj időben jegyet!
+        </p>
+        {performances && <PerformancesNextWeek performances={performances} />}
+      </div>
+
+      {/* Bérletvásárlás */}
+      <div className="max-w-6xl w-full mx-auto bg-c-secondary-dark bg-opacity-10 shadow-lg rounded-2xl p-12 my-16 px-6 sm:px-12 lg:px-20">
+        <h2 className="text-4xl font-bold text-c-text mb-6">Hogyan tudsz jegyet foglalni?</h2>
+        <p className="text-lg text-c-text/80 mb-6">
+          Nézz szét bérleteink között, válaszd ki, amelyik tetszik, vedd meg, és foglalj előadást!
+        </p>
+        <DefaultButton onClick={() => navigate('/season-tickets')} text="Vásárolj bérletet" />
+      </div>
+
+      {/* Támogatás */}
+      <div className="max-w-6xl w-full mx-auto bg-c-secondary-dark bg-opacity-10 shadow-lg rounded-2xl p-12 my-16 px-6 sm:px-12 lg:px-20">
+        <h2 className="text-4xl font-bold text-c-text mb-6">Nem állunk meg itt!</h2>
+        <p className="text-lg text-c-text/80 mb-6">
+          A BreakThe4th oldalon támogathatod kedvenc csapataid előadásterveit! Válj te is mecénássá!
+        </p>
+        {performances && <ListingFuturePerformances performances={performances} />}
+      </div>
+      <div className="max-w-6xl w-full mx-auto bg-c-secondary-dark bg-opacity-10 shadow-lg rounded-2xl p-12 my-16 px-6 sm:px-12 lg:px-20">
+        <h2 className="text-4xl font-bold text-c-text mb-6">Részletes keresés</h2>
+        <p className="text-lg text-c-text/80 mb-6">
+          Itt megtalálsz mindent, keress dátum, színház szerint. Pipáld csak be a tervezett
+          előadásokat és már segítheted is a kedvenc színházadat, hogy létrehozza az új bemutatóját!
+        </p>
+        <DefaultButton onClick={() => navigate('/performances')} text="Keress" />
       </div>
     </>
   );
