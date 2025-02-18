a2e473c32c93f0d082310b9229a594c67bdc570f
ViktorBazsi <viktor.balazs.endre@gmail.com>
2025-02-18 23:03:02 +0100

    Done - backend for Creators Basic CRUDs no validation.

--- frontend/src/services/creators.service.js
+++ frontend/src/services/creators.service.js
@@ -2,7 +2,7 @@ import axiosInstance from './axiosInstance';
 
 const getCreators = async () => {
   try {
-    const response = await axiosInstance.get('/api/dropdown-data-creators');
+    const response = await axiosInstance.get('/api/creators/dropdown-data-creators');
     return response.data; // Successful answer
   } catch (error) {
     throw error.response ? error.response.data : error; // In case of error
